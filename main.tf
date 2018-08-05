d6fc89ada0e9806135207b4a3d990d8e3247dac8
itsdalmo <kristian@doingit.no>
2018-08-05 21:48:45 +0200

    Add final snapshot name.

--- main.tf
+++ main.tf
@@ -1,23 +1,29 @@
 # -------------------------------------------------------------------------------
 # Resources
 # -------------------------------------------------------------------------------
+resource "random_string" "suffix" {
+  length  = 8
+  special = "false"
+}
+
 resource "aws_db_instance" "main" {
-  depends_on             = ["aws_db_subnet_group.main"]
-  identifier             = "${var.name_prefix}-db"
-  name                   = "${var.database_name}"
-  username               = "${var.username}"
-  password               = "${var.password}"
-  port                   = "${var.port}"
-  engine                 = "${var.engine}"
-  engine_version         = "${var.engine_version}"
-  instance_class         = "${var.instance_type}"
-  storage_type           = "gp2"
-  allocated_storage      = "${var.allocated_storage}"
-  snapshot_identifier    = "${var.snapshot_identifier}"
-  skip_final_snapshot    = "${var.skip_final_snapshot}"
-  publicly_accessible    = "${var.publicly_accessible}"
-  vpc_security_group_ids = ["${aws_security_group.main.id}"]
-  multi_az               = "${var.multi_az}"
+  depends_on                = ["aws_db_subnet_group.main"]
+  identifier                = "${var.name_prefix}-db"
+  name                      = "${var.database_name}"
+  username                  = "${var.username}"
+  password                  = "${var.password}"
+  port                      = "${var.port}"
+  engine                    = "${var.engine}"
+  engine_version            = "${var.engine_version}"
+  instance_class            = "${var.instance_type}"
+  storage_type              = "gp2"
+  allocated_storage         = "${var.allocated_storage}"
+  snapshot_identifier       = "${var.snapshot_identifier}"
+  final_snapshot_identifier = "${var.name_prefix}-final-${random_string.suffix.id}"
+  skip_final_snapshot       = "${var.skip_final_snapshot}"
+  publicly_accessible       = "${var.publicly_accessible}"
+  vpc_security_group_ids    = ["${aws_security_group.main.id}"]
+  multi_az                  = "${var.multi_az}"
 
   # NOTE: This is duplicated because subnet_group does not return the name.
   db_subnet_group_name = "${var.name_prefix}-subnet-group"
