28e64ff2da54d5e8409850c490818312f8ab739c
itsdalmo <kristian@doingit.no>
2018-10-01 14:38:43 +0200

    Rename address to endpoint (to align with rds_cluster)

--- outputs.tf
+++ outputs.tf
@@ -32,11 +32,6 @@ output "subnet_group_arn" {
 }
 
 output "endpoint" {
-  description = "The connection endpoint in address:port format."
-  value       = "${aws_db_instance.main.endpoint}"
-}
-
-output "address" {
   description = "The hostname of the RDS instance."
   value       = "${aws_db_instance.main.address}"
 }
