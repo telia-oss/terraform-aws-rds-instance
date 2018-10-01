4508774c1117b8ac6d6941061e56af0f3c24ec48
itsdalmo <kristian@doingit.no>
2018-10-01 14:37:22 +0200

    Remove unused outputs.

--- outputs.tf
+++ outputs.tf
@@ -16,29 +16,19 @@ output "arn" {
   value       = "${aws_db_instance.main.arn}"
 }
 
-output "availability_zone" {
-  description = "The availability zone of the RDS instance"
-  value       = "${aws_db_instance.main.availability_zone}"
-}
-
-output "zone_id" {
-  description = "The canonical hosted zone ID of the DB instance (to be used in a Route 53 Alias record)"
-  value       = "${aws_db_instance.main.hosted_zone_id}"
-}
-
 output "security_group_id" {
   description = "The ID of the security group."
   value       = "${aws_security_group.main.id}"
 }
 
-output "subnet_group_arn" {
-  description = "The ARN of the db subnet group."
-  value       = "${aws_db_subnet_group.main.arn}"
-}
-
 output "subnet_group_id" {
   description = "The db subnet group name."
   value       = "${aws_db_subnet_group.main.id}"
+}
+
+output "subnet_group_arn" {
+  description = "The ARN of the db subnet group."
+  value       = "${aws_db_subnet_group.main.arn}"
 }
 
 output "endpoint" {
