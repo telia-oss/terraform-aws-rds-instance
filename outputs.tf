47c474b4a817a0d083b48e46c5468a47680e94f9
itsdalmo <kristian@doingit.no>
2018-10-01 12:43:27 +0200

    Align outputs of RDS modules.

--- outputs.tf
+++ outputs.tf
@@ -6,44 +6,9 @@ output "id" {
   value       = "${aws_db_instance.main.id}"
 }
 
-output "endpoint" {
-  description = "The DNS address of the RDS instance."
-  value       = "${aws_db_instance.main.endpoint}"
-}
-
-output "postgres_connection_string" {
-  description = "PostgreSQL connection string."
-  value       = "postgres://${var.username}:${var.password}@${aws_db_instance.main.address}:${var.port}/main"
-}
-
-output "port" {
-  description = "The port on which the DB accepts connections."
-  value       = "${aws_db_instance.main.port}"
-}
-
-output "database_name" {
-  description = "Name for the automatically created database."
-  value       = "${aws_db_instance.main.name}"
-}
-
-output "security_group_id" {
-  description = "The ID of the security group."
-  value       = "${aws_security_group.main.id}"
-}
-
-output "subnet_group_arn" {
-  description = "The ARN of the db subnet group."
-  value       = "${aws_db_subnet_group.main.arn}"
-}
-
-output "subnet_group_id" {
-  description = "The db subnet group name."
-  value       = "${aws_db_subnet_group.main.id}"
-}
-
-output "address" {
-  description = "The address of the RDS instance"
-  value       = "${aws_db_instance.main.address}"
+output "resource_id" {
+  description = "The RDS Resource ID of this instance"
+  value       = "${aws_db_instance.main.resource_id}"
 }
 
 output "arn" {
@@ -61,12 +26,42 @@ output "zone_id" {
   value       = "${aws_db_instance.main.hosted_zone_id}"
 }
 
-output "resource_id" {
-  description = "The RDS Resource ID of this instance"
-  value       = "${aws_db_instance.main.resource_id}"
+output "security_group_id" {
+  description = "The ID of the security group."
+  value       = "${aws_security_group.main.id}"
+}
+
+output "subnet_group_arn" {
+  description = "The ARN of the db subnet group."
+  value       = "${aws_db_subnet_group.main.arn}"
+}
+
+output "subnet_group_id" {
+  description = "The db subnet group name."
+  value       = "${aws_db_subnet_group.main.id}"
+}
+
+output "endpoint" {
+  description = "The connection endpoint in address:port format."
+  value       = "${aws_db_instance.main.endpoint}"
+}
+
+output "address" {
+  description = "The hostname of the RDS instance."
+  value       = "${aws_db_instance.main.address}"
+}
+
+output "port" {
+  description = "The database port."
+  value       = "${aws_db_instance.main.port}"
 }
 
 output "username" {
-  description = "The master username for the database"
+  description = "Username for the master DB user."
   value       = "${aws_db_instance.main.username}"
+}
+
+output "database_name" {
+  description = "The database name."
+  value       = "${aws_db_instance.main.name}"
 }
